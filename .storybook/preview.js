541f63a99ef5e6321f9c5950cc8e748b17e42d41
MarvinxKiyingi <marvin96k@gmail.com>
2022-10-15 18:03:25 +0200

    feat(storybook): initial storybook setup

---
+++ .storybook/preview.js
@@ -0,0 +1,36 @@
+const BREAKPOINTS_INT = {
+  xs: 0,
+  sm: 600,
+  md: 900,
+  lg: 1200,
+  xl: 1536,
+};
+
+const viewports = Object.fromEntries(
+  Object.entries(BREAKPOINTS_INT).map(([key, val], idx) => {
+    return [
+      key,
+      {
+        name: key,
+        styles: {
+          width: `${val}px`,
+          height: `${(idx + 5) * 10}vh`,
+        },
+      },
+    ];
+  })
+);
+
+export const parameters = {
+  layout: 'fullscreen',
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  viewport: {
+    viewports,
+  },
+};
