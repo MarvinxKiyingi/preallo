ef8aaec0b821699f2cd3aa2646c03fb55d8825f0
MarvinxKiyingi <marvin96k@gmail.com>
2022-11-17 19:35:11 +0100

    fix(AddButton): refactor font size to work dynamically both in mobile and desktop

--- src/components/AddButton/AddButton.tsx
+++ src/components/AddButton/AddButton.tsx
@@ -1,4 +1,4 @@
-import { IconButton, IconButtonProps as MuiIconButtonProps, styled } from '@mui/material';
+import { IconButton, IconButtonProps as MuiIconButtonProps } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import { theme } from '../../styles/muiTheme';
 
@@ -6,25 +6,22 @@ import { theme } from '../../styles/muiTheme';
 type IconButtonProps = Pick<MuiIconButtonProps, 'size' | 'color' | 'disableRipple' | 'disabled' | 'disableFocusRipple'>;
 
 export interface IAddButtonProps extends IconButtonProps {
-  fontSize?: string;
+  fontSizeMobile?: string;
+  fontSizeDesktop?: string;
   hasBgColor?: boolean;
 }
 
-const StyledIconButton = styled(IconButton)(({ theme }) => ({
-  fontSize: theme.spacing(4),
-  [theme.breakpoints.up('md')]: {
-    fontSize: theme.spacing(5),
-  },
-}));
-
-export const AddButton = ({ fontSize, hasBgColor, ...props }: IAddButtonProps) => {
+export const AddButton = ({ fontSizeMobile, fontSizeDesktop, hasBgColor, ...props }: IAddButtonProps) => {
   const iconButtonInlineStyling = {
-    fontSize: fontSize ? fontSize : null,
+    fontSize: fontSizeMobile ? fontSizeMobile : theme.spacing(3),
+    [theme.breakpoints.up('md')]: {
+      fontSize: fontSizeDesktop ? fontSizeDesktop : theme.spacing(6),
+    },
     backgroundColor: hasBgColor ? theme.palette.common.white : null,
   };
   return (
-    <StyledIconButton sx={iconButtonInlineStyling} {...props}>
+    <IconButton sx={iconButtonInlineStyling} {...props}>
       <AddIcon fontSize='inherit' />
-    </StyledIconButton>
+    </IconButton>
   );
 };
