5ff9494fa98fc68168860211420684320a46af38
MarvinxKiyingi <marvin96k@gmail.com>
2022-11-20 01:52:20 +0100

    style(Icons): added som icons

---
+++ src/components/SvgIcon/SvgIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SvgIcon as MuiSvgIcon, SvgIconProps } from '@mui/material';
+
+export type IMuiSvgIconProps = Pick<SvgIconProps, 'children' | 'color' | 'fontSize' | 'sx' | 'viewBox'>;
+
+export interface ISvgProps extends IMuiSvgIconProps {}
+
+export const SvgIcon = ({ children, ...props }: IMuiSvgIconProps) => {
+  return <MuiSvgIcon {...props}>{children}</MuiSvgIcon>;
+};
