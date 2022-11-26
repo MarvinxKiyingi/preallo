b36a5e76dd0f2e22631c7d6394459c379b264cc5
MarvinxKiyingi <marvin96k@gmail.com>
2022-11-26 22:00:11 +0100

    style(CircularProgress): styled component

---
+++ src/components/CircularProgress/CircularProgress.stories.tsx
@@ -0,0 +1,22 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { CircularProgress } from './CircularProgress';
+
+export default {
+  title: 'components/CircularProgress',
+  component: CircularProgress,
+  argTypes: {
+    progressTextColor: {
+      options: ['common.white', 'common.black', 'inherit', 'primary', 'secondary', 'error', 'info', 'success', 'warning'],
+      control: { type: 'select' },
+    },
+  },
+  args: {
+    value: 75,
+    size: 230,
+    progressTextColor: 'primary',
+    circularProgressColor: 'secondary',
+  },
+} as ComponentMeta<typeof CircularProgress>;
+const Template: ComponentStory<typeof CircularProgress> = (args) => <CircularProgress {...args} />;
+
+export const Default = Template.bind({});
---
+++ src/components/CircularProgress/CircularProgress.tsx
@@ -0,0 +1,54 @@
+import { CircularProgress as MuiCircularProgress, CircularProgressProps as MuiCircularProgressProps, Typography, Box, styled } from '@mui/material';
+
+export interface IMuiCircularProgressProps extends MuiCircularProgressProps {
+  value: number;
+  thickness?: number;
+  size?: number;
+  circularProgressColor: 'inherit' | 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning' | undefined;
+  progressTextColor: 'common.white' | 'common.black' | 'inherit' | 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning' | string;
+}
+
+const StyledCircularProgress = styled(Typography)(({ theme }) => ({
+  '.circularProgress': {
+    '&-progressText': {
+      '&-text': {
+        [theme.breakpoints.up('md')]: {
+          ...theme.typography.h1,
+          fontWeight: 400,
+        },
+      },
+    },
+  },
+}));
+
+export const CircularProgress = ({ value, circularProgressColor, progressTextColor, ...props }: IMuiCircularProgressProps) => {
+  const hasValue = value ? value : 0;
+  return (
+    <StyledCircularProgress className='circularProgress-container' sx={{ position: 'relative', display: 'inline-flex' }}>
+      <MuiCircularProgress
+        className='circularProgress-circularProgress'
+        variant='determinate'
+        value={hasValue}
+        color={circularProgressColor}
+        {...props}
+      />
+      <Box
+        className='circularProgress-progressText-container'
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography className='circularProgress-progressText-text' variant='caption' component='div' color={progressTextColor}>{`${Math.round(
+          hasValue
+        )}%`}</Typography>
+      </Box>
+    </StyledCircularProgress>
+  );
+};
