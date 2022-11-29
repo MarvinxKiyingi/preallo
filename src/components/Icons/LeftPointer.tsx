ff7c47dfa17ae6cae63645709d33779323d3040a
MarvinxKiyingi <marvin96k@gmail.com>
2022-11-29 16:23:33 +0100

    feat(Icons): updated some icons

--- src/components/Icons/LeftPointer.tsx
+++ src/components/Icons/LeftPointer.tsx
@@ -2,8 +2,8 @@ import { ISvgProps, SvgIcon } from '../SvgIcon/SvgIcon';
 
 const LeftPointer = (props: ISvgProps) => {
   return (
-    <SvgIcon {...props} sx={{ fill: 'none' }}>
-      <path d='M16 20L8 12L16 4' stroke='black' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
+    <SvgIcon {...props}>
+      <path d='M9.47 8.47a.75.75 0 0 0 1.06 1.06L9.47 8.47Zm6.06-3.94a.75.75 0 0 0-1.06-1.06l1.06 1.06Zm-1.06 16a.75.75 0 1 0 1.06-1.06l-1.06 1.06Zm-6.94-9.06a.75.75 0 0 0-1.06 1.06l1.06-1.06Zm3-1.94 5-5-1.06-1.06-5 5 1.06 1.06Zm5 9.94-8-8-1.06 1.06 8 8 1.06-1.06Z' />
     </SvgIcon>
   );
 };
--- src/components/Icons/PlusIcon.tsx
+++ src/components/Icons/PlusIcon.tsx
@@ -3,7 +3,7 @@ import { ISvgProps, SvgIcon } from '../SvgIcon/SvgIcon';
 const PlusIcon = (props: ISvgProps) => {
   return (
     <SvgIcon {...props}>
-      <path d='M12 4V20M4 12H20' stroke='black' stroke-width='1.7' stroke-linecap='round' stroke-linejoin='round' />
+      <path d='M4 11.25a.75.75 0 0 0 0 1.5v-1.5Zm5 1.5a.75.75 0 0 0 0-1.5v1.5Zm3-1.5a.75.75 0 0 0 0 1.5v-1.5Zm8 1.5a.75.75 0 0 0 0-1.5v1.5ZM12.75 4a.75.75 0 0 0-1.5 0h1.5Zm-1.5 16a.75.75 0 0 0 1.5 0h-1.5ZM4 12.75h5v-1.5H4v1.5Zm8 0h8v-1.5h-8v1.5ZM11.25 4v16h1.5V4h-1.5Z' />
     </SvgIcon>
   );
 };
