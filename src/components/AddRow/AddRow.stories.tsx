683d40c54c1f0dec2c4a4560b0290a93cd8e9332
Marvin <marvin96k@gmail.com>
2023-03-18 00:48:01 +0100

    fix(FilterBar): add filter Chips

--- src/components/AddRow/AddRow.stories.tsx
+++ src/components/AddRow/AddRow.stories.tsx
@@ -9,9 +9,27 @@ export default {
     fontSizeMobile: '1.5rem',
     fontSizeDesktop: '2rem',
     size: 'small',
-    color: 'inherit',
+    color: 'primary',
     renewIsVisible: true,
     addIsVisible: true,
+    filter: true,
+    chipsList: [
+      {
+        id: 'Test 1',
+        activated: true,
+        chipLabel: 'Test First',
+      },
+      {
+        id: 'Test 2',
+        activated: false,
+        chipLabel: 'Test Second',
+      },
+      {
+        id: 'Test 3',
+        activated: false,
+        chipLabel: 'Test Third',
+      },
+    ],
   },
 } as ComponentMeta<typeof AddRow>;
 const Template: ComponentStory<typeof AddRow> = (args) => <AddRow {...args} />;
