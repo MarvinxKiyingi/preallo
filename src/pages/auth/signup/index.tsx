71724bb8010fee3d0d4392da7106f224fe34ba26
Marvin <marvin96k@gmail.com>
2023-04-07 11:49:40 +0200

    feat(SignUp): ability to sign up a user with email

--- src/pages/auth/signup/index.tsx
+++ src/pages/auth/signup/index.tsx
@@ -1,18 +1,45 @@
-import { Stack, TextField, Typography } from '@mui/material';
+import { Box, Link, Stack, TextField, Typography, styled } from '@mui/material';
 import { AuthLayout } from '../../../components/Layouts/AuthLayout';
-import {
-  CtaStack,
-  Description,
-  Email,
-  Google,
-  SignInLink,
-  Title,
-} from './SignUp.styles';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { GoogleIcon } from '../../../components/Icons';
 import { ISignUp } from '../../../model/ISignUp';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { ISignUpYupSchema } from '../../../model/IYupSchema';
+import { useAuth } from '../../../utils/context/AuthContext';
+import { grey } from '../../../styles/colors/grey';
+import { Button } from '../../../components/Button/Button';
+
+export const Title = styled(Typography)(({ theme }) => ({
+  ...theme.typography.h1,
+}));
+
+export const Description = styled(Typography)(({ theme }) => ({
+  ...theme.typography.body2,
+  color: grey.dark[500],
+}));
+
+export const Email = styled(Button)({});
+
+export const CtaStack = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  marginTop: theme.spacing(3),
+  gap: theme.spacing(3),
+  textAlign: 'center',
+}));
+
+export const ForgottenPassword = styled(Link)(({ theme }) => ({
+  ...theme.typography.body2,
+  textDecoration: 'none',
+  display: 'flex',
+  alignSelf: 'end',
+  color: theme.palette.secondary.main,
+}));
+
+export const SignInLink = styled(Link)(({ theme }) => ({
+  ...theme.typography.body2,
+  fontWeight: 700,
+  color: theme.palette.secondary.main,
+}));
 
 const SignUp = () => {
   const {
@@ -21,8 +48,11 @@ const SignUp = () => {
     formState: { errors },
   } = useForm<ISignUp>({ resolver: yupResolver(ISignUpYupSchema) });
 
+  const { signUpUser } = useAuth();
+
   const formSubmitHandler: SubmitHandler<ISignUp> = (data: ISignUp) => {
     console.log('data:', data);
+    signUpUser(data);
   };
 
   return (
@@ -87,21 +117,12 @@ const SignUp = () => {
             variant={'contained'}
             component='button'
           >
-            Sign in
+            Sign up
           </Email>
 
-          <Google
-            fullHeight={false}
-            fullWidth
-            variant={'contained'}
-            component='button'
-            startIcon={<GoogleIcon />}
-          >
-            Sign in with google
-          </Google>
-
           <Typography pb={4}>
-            Already have an account? <SignInLink href={'#'}>Sign In</SignInLink>
+            Already have an account?{' '}
+            <SignInLink href={'/auth/signin'}>Sign In</SignInLink>
           </Typography>
         </CtaStack>
       </form>
