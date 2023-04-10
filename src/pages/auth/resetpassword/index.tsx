92fb49064f937a5dce6cca098585b4667af13736
Marvin <marvin96k@gmail.com>
2023-04-10 19:08:52 +0200

    feat(resetPassword): Added header

--- src/pages/auth/resetpassword/index.tsx
+++ src/pages/auth/resetpassword/index.tsx
@@ -8,6 +8,7 @@ import { IPasswordReset } from '../../../model/IPasswordReset';
 import { IPasswordResetYupSchema } from '../../../model/IYupSchema';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useAuth } from '../../../utils/context/AuthContext';
+import Head from 'next/head';
 
 export const Title = styled(Typography)(({ theme }) => ({
   ...theme.typography.h2,
@@ -53,52 +54,58 @@ const ResetPassword = () => {
     passwordReset(data);
   };
 
+  const description =
+    'Type in your user email to receive instructions on how to reset your password.';
+
   return (
-    <AuthLayout>
-      <form onSubmit={handleSubmit(formSubmitHandler)}>
-        <Stack spacing={1} direction='column' mb={6}>
-          <Title as={'h1'}>
-            Password
-            <br />
-            reset
-          </Title>
-          <Description>
-            Type in your user email to receive instructions on how to reset your
-            password.
-          </Description>
-        </Stack>
-
-        <Stack spacing={2} direction='column'>
-          <TextField
-            {...register('email')}
-            error={!!errors.email}
-            helperText={errors.email ? errors.email?.message : ''}
-            label='Email'
-            type='email'
-            margin={'none'}
-          />
-        </Stack>
-
-        <CtaStack>
-          <Email
-            type='submit'
-            fullHeight={false}
-            fullWidth
-            variant={'contained'}
-            component='button'
-          >
-            Reset password
-          </Email>
-
-          <Stack spacing={1}>
-            <Typography>
-              Already have an account?{' '}
-              <SignInLink href={'/auth/signin'}>Sign In</SignInLink>
-            </Typography>
+    <>
+      <Head>
+        <title>Password reset</title>
+        <meta name='description' content={description} />
+      </Head>
+      <AuthLayout>
+        <form onSubmit={handleSubmit(formSubmitHandler)}>
+          <Stack spacing={1} direction='column' mb={6}>
+            <Title as={'h1'}>
+              Password
+              <br />
+              reset
+            </Title>
+            <Description>{description}</Description>
           </Stack>
-        </CtaStack>
-      </form>
-    </AuthLayout>
+
+          <Stack spacing={2} direction='column'>
+            <TextField
+              {...register('email')}
+              error={!!errors.email}
+              helperText={errors.email ? errors.email?.message : ''}
+              label='Email'
+              type='email'
+              margin={'none'}
+            />
+          </Stack>
+
+          <CtaStack>
+            <Email
+              type='submit'
+              fullHeight={false}
+              fullWidth
+              variant={'contained'}
+              component='button'
+            >
+              Reset password
+            </Email>
+
+            <Stack spacing={1}>
+              <Typography>
+                Already have an account?{' '}
+                <SignInLink href={'/auth/signin'}>Sign In</SignInLink>
+              </Typography>
+            </Stack>
+          </CtaStack>
+        </form>
+      </AuthLayout>
+    </>
   );
 };
 
