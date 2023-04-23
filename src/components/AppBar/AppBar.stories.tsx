d9a507bf808da7beec1240cc0dab9ca4290e04be
Marvin <marvin96k@gmail.com>
2023-04-23 12:27:57 +0200

    refactor(AppBar): name change

--- src/components/AppBar/AppBar.stories.tsx
+++ src/components/AppBar/AppBar.stories.tsx
@@ -1,19 +1,29 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { AppBar } from './AppBar';
+import { AppBar, IAppBar } from './AppBar';
+import { IAvatar } from '../Avatar/Avatar';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   title: 'components/AppBar',
   component: AppBar,
   args: {
     fontSizeMobile: '1.5rem',
     fontSizeDesktop: '2rem',
-    dashBoardIsVisible: true,
-    profileIsVisible: true,
+    hideDashBoard: false,
+    hideProfile: false,
     title: 'Dashboard',
     url: 'https://assets.teenvogue.com/photos/60883d3762144bb77196700d/1:1/w_595,h_595,c_limit/unnamed%20(8).jpg',
   },
-} as ComponentMeta<typeof AppBar>;
+};
 
-const Template: ComponentStory<typeof AppBar> = (args) => <AppBar {...args} />;
+export const Primary = (args: IAppBar & IAvatar) => <AppBar {...args} />;
+Primary.args = {};
 
-export const Default = Template.bind({});
+export const HideDashBar = (args: IAppBar & IAvatar) => <AppBar {...args} />;
+HideDashBar.args = {
+  hideDashBoard: true,
+};
+
+export const HideProfile = (args: IAppBar & IAvatar) => <AppBar {...args} />;
+HideProfile.args = {
+  hideProfile: true,
+};
