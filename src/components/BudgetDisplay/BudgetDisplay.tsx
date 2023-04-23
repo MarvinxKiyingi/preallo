4cf3aeb15b6eaf708ffc72766a173cb7c81ab2d1
Marvin <marvin96k@gmail.com>
2023-04-23 11:43:08 +0200

    style(BudgetDisplay): replace a version

--- src/components/BudgetDisplay/BudgetDisplay.tsx
+++ src/components/BudgetDisplay/BudgetDisplay.tsx
@@ -1,61 +1,92 @@
-import { Box, LinearProgress, LinearProgressProps, styled, Typography } from '@mui/material';
+import {
+  Box,
+  LinearProgress,
+  LinearProgressProps,
+  styled,
+  Typography,
+} from '@mui/material';
 import { grey } from '../../styles/colors/grey';
 
 export type IBudgetDisplay = {
   bgColor?: string;
   amount: number;
-  progressValue: number;
+  progressValue?: number;
   variant?: 'buffer' | 'determinate' | 'indeterminate' | 'query';
   viewProgress?: boolean;
   fullWidth: boolean;
   days: number;
-  centerWithTitle: boolean;
+  version: 'primary' | 'secondary';
+  title?: string;
 };
 
-const StyledBudgetDisplay = styled(Box)<{ ownerState: IBudgetDisplay }>(({ theme, ownerState }) => ({
-  backgroundColor: ownerState.bgColor ? ownerState.bgColor : theme.palette.primary.main,
-  color: theme.palette.common.white,
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  gap: theme.spacing(),
-  borderRadius: theme.spacing(2),
-  padding: theme.spacing(3),
-  width: ownerState.fullWidth ? '100%' : 334,
-  height: '100%',
-  [theme.breakpoints.up('lg')]: {
-    width: ownerState.fullWidth ? '100%' : 663,
+const StyledBudgetDisplay = styled(Box)<{ ownerState: IBudgetDisplay }>(
+  ({ theme, ownerState }) => ({
+    backgroundColor: ownerState.bgColor
+      ? ownerState.bgColor
+      : theme.palette.primary.main,
+    color: theme.palette.common.white,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: theme.spacing(),
+    borderRadius: theme.spacing(2),
+    padding: theme.spacing(3),
+    width: ownerState.fullWidth ? '100%' : 334,
+    height: '100%',
+    [theme.breakpoints.up('lg')]: {
+      width: ownerState.fullWidth ? '100%' : 663,
+    },
+
+    '.textContainer': {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      width: '100%',
+      height: '100%',
+
+      '.infoText': {
+        color: grey.light[500],
+      },
+      '.title': {
+        color: theme.palette.text.secondary,
+        ...theme.typography.subtitle1,
+        fontWeight: 600,
+      },
+
+      '.amount': {
+        ...theme.typography.h6,
+        fontWeight: 700,
+        textTransform: 'uppercase',
+      },
+    },
+
+    '.linearProgress-container': {
+      width: '100%',
+      span: {
+        borderRadius: theme.spacing(),
+      },
+    },
+  })
+);
+
+const StyledSecondaryBudgetDisplay = styled(Box)(({ theme }) => ({
+  '.title': {
+    fontWeight: 600,
   },
 
   '.textContainer': {
     display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    width: '100%',
-    height: '100%',
-    alignItems: ownerState.centerWithTitle ? 'center' : 'initial',
+    justifyContent: 'space-between',
+    alignItems: 'center',
 
     '.infoText': {
-      color: grey.light[500],
-    },
-    '.title': {
-      color: ownerState.centerWithTitle ? grey.light[500] : theme.palette.text.secondary,
-      ...theme.typography.subtitle1,
-      fontWeight: 600,
-      lineHeight: 'null',
+      color: grey.shades[600],
     },
 
     '.amount': {
-      ...theme.typography.h6,
+      ...theme.typography.h5,
       fontWeight: 700,
       textTransform: 'uppercase',
-    },
-  },
-
-  '.linearProgress-container': {
-    width: '100%',
-    span: {
-      borderRadius: theme.spacing(),
     },
   },
 }));
@@ -67,7 +98,8 @@ export const BudgetDisplay = ({
   days,
   amount,
   progressValue,
-  centerWithTitle,
+  version,
+  title,
   ...props
 }: IBudgetDisplay & LinearProgressProps) => {
   const ownerState = {
@@ -76,22 +108,57 @@ export const BudgetDisplay = ({
     amount,
     days,
     progressValue,
-    centerWithTitle,
+    version,
+    title,
   };
   return (
-    <StyledBudgetDisplay className='budgetDisplay-container' ownerState={ownerState} {...props}>
-      <div className='textContainer'>
-        <Typography className={centerWithTitle ? 'title' : 'infoText'} variant='caption'>
-          {centerWithTitle ? 'Total' : `Left to spend, for the next ${days} days`}
-        </Typography>
-        <Typography className='amount' variant='h4'>{`${amount} kr`}</Typography>
-      </div>
+    <>
+      {version === 'secondary' ? (
+        <StyledSecondaryBudgetDisplay
+          className='budgetDisplay-secondary'
+          {...props}
+        >
+          <Typography className='title' variant='h4'>
+            {title}
+          </Typography>
 
-      {viewProgress && (
-        <Box className='linearProgress-container'>
-          <LinearProgress sx={{ height: 6 }} value={progressValue} {...props} />
-        </Box>
+          <div className='textContainer'>
+            <Typography className={'infoText'} variant='body1'>
+              {`Left to spend, for the next ${days} days`}
+            </Typography>
+            <Typography
+              className='amount'
+              variant='h5'
+            >{`${amount} kr`}</Typography>
+          </div>
+        </StyledSecondaryBudgetDisplay>
+      ) : (
+        <StyledBudgetDisplay
+          className='budgetDisplay-primary'
+          ownerState={ownerState}
+          {...props}
+        >
+          <div className='textContainer'>
+            <Typography className={'infoText'} variant='caption'>
+              {`Left to spend, for the next ${days} days`}
+            </Typography>
+            <Typography
+              className='amount'
+              variant='h4'
+            >{`${amount} kr`}</Typography>
+          </div>
+
+          {viewProgress && (
+            <Box className='linearProgress-container'>
+              <LinearProgress
+                sx={{ height: 6 }}
+                value={progressValue}
+                {...props}
+              />
+            </Box>
+          )}
+        </StyledBudgetDisplay>
       )}
-    </StyledBudgetDisplay>
+    </>
   );
 };
