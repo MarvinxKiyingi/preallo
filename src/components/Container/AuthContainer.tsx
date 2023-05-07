166068d2434b706e17f5d8289983fdf4f5e648be
Marvin <marvin96k@gmail.com>
2023-05-07 10:22:25 +0200

    fix(Container): use existing container

--- src/components/Container/AuthContainer.tsx
+++ src/components/Container/AuthContainer.tsx
@@ -1,26 +1,7 @@
 import React from 'react';
 import { IChildren } from '../../model/IChildren';
-import { styled } from '@mui/material';
-
-const Container = styled('main')(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  minHeight: '100vh',
-  backgroundColor: theme.palette.background.default,
-  padding: theme.spacing(3, 3, 0, 3),
-
-  [theme.breakpoints.up('sm')]: {
-    padding: 'unset',
-  },
-
-  [theme.breakpoints.up('md')]: {
-    height: '100vh',
-    display: 'grid',
-    padding: 'unset',
-    gridTemplateColumns: '1.3fr 1fr',
-  },
-}));
+import Container from './Container';
 
 export const AuthContainer = ({ children }: IChildren) => {
-  return <Container>{children}</Container>;
+  return <Container desktopColumns='1.3fr 1fr'>{children}</Container>;
 };
--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,29 +1,40 @@
-import { Box, styled } from '@mui/material';
+import { styled } from '@mui/material';
 import { IChildren } from '../../model/IChildren';
 
-const StyledContainer = styled(Box)(({ theme }) => ({
+interface IContainer extends IChildren {
+  /** Add css string to adjust the gird template column*/
+  desktopColumns?: string;
+}
+
+const StyledContainer = styled('main')<{
+  ownerState: IContainer;
+}>(({ theme, ownerState }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  minHeight: '100vh',
   backgroundColor: theme.palette.background.default,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-const StyledWrapper = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  width: '100%',
-  maxWidth: '87.5rem',
-  padding: theme.spacing(0, 3),
+  padding: theme.spacing(3, 3, 0, 3),
+
+  [theme.breakpoints.up('sm')]: {
+    padding: 'unset',
+  },
 
   [theme.breakpoints.up('md')]: {
+    height: '100vh',
+    display: 'grid',
     padding: 'unset',
-    height: '100%',
+    gridTemplateColumns: ownerState.desktopColumns
+      ? ownerState.desktopColumns
+      : '0.4fr 1fr',
   },
 }));
-const Container = ({ children }: IChildren) => {
-  return (
-    <StyledContainer>
-      <StyledWrapper>{children}</StyledWrapper>
-    </StyledContainer>
-  );
+
+const Container = ({ children, desktopColumns = '0.4fr 1fr' }: IContainer) => {
+  const ownerState = {
+    children,
+    desktopColumns,
+  };
+  return <StyledContainer ownerState={ownerState}>{children}</StyledContainer>;
 };
 
 export default Container;
