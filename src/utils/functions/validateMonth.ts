fe8479b0b7df3ce3e7d6940a43ffdc2aa061df78
Marvin <marvin96k@gmail.com>
2023-06-18 21:30:12 +0200

    refactor: clean up

--- src/utils/functions/validateMonth.ts
+++
@@ -1,9 +0,0 @@
-import { monthList } from '../Variables/monthList';
-
-export const validateMonth = (month: number) => {
-  const currentMonthNumber = new Date().getMonth();
-  const currentMonthName = monthList[currentMonthNumber];
-
-  console.log(currentMonthName);
-  console.log(month);
-};
