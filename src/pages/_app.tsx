59f81d8e7c8d3c5d6435d7cc03df4af3a2ce95aa
Marvin <marvin96k@gmail.com>
2023-07-28 15:55:12 +0200

    feat(Auth): config google adapter and protected routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,19 @@
 import '../styles/globals.css';
-import { memo, useEffect, useState } from 'react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from '../styles/theme/muiTheme';
-import { useRouter } from 'next/router';
 import { HeadContainer } from '../components/Container/HeadContainer';
 import { SessionProvider } from 'next-auth/react';
+import ProtectedRoutes from '../utils/ProtectedRoutes';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  const router = useRouter();
-
   return (
     <SessionProvider session={session}>
       <ThemeProvider theme={theme}>
-        <HeadContainer />
-        <Component {...pageProps} />
+        <ProtectedRoutes>
+          <HeadContainer />
+          <Component {...pageProps} />
+        </ProtectedRoutes>
       </ThemeProvider>
     </SessionProvider>
   );
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { FirestoreAdapter } from '@auth/firebase-adapter';
+import { cert } from 'firebase-admin/app';
+import { Adapter } from 'next-auth/adapters';
 
 export default NextAuth({
   providers: [
@@ -8,4 +11,12 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
     }),
   ],
+
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.CLIENT_EMAIL!,
+      privateKey: process.env.PRIVATE_KEY!.replace(/\\n/g, '\n'),
+    }),
+  }) as Adapter,
 });
