523b7dee16f166ef3088488603eee2e1b61a637b
Marvin <marvin96k@gmail.com>
2023-07-31 14:23:09 +0200

    feat(month): switch out current user with sessions

--- src/model/IMonthPage.ts
+++ src/model/IMonthPage.ts
@@ -1,6 +1,6 @@
-import { User } from '@firebase/auth';
+import { Session } from 'next-auth';
 import { IMonth } from './IMonth';
 
 export type IMonthPage = IMonth & {
-  currentUser: User | null | undefined;
+  session: Session;
 };
